903fce739bab8ec6b635f6b2cc7b73d4f6954666
Beknur Tumenov <veryboredbeko@gmail.com>
2025-10-08 00:34:47 +0500

    update readme.md

---
+++ broker-service/src/main/java/com/example/broker_service/Controller.java
@@ -0,0 +1,13 @@
+package com.example.broker_service;
+
+public class Controller {
+
+    private final Observer observer;
+
+    public Controller(Observer observer) {
+        this.observer = observer;
+    }
+
+    // TODO: HTTP POST /subscribe
+    // TODO: HTTP POST /publish
+}
--- broker-service/src/main/java/com/example/broker_service/Observer.java
+++ broker-service/src/main/java/com/example/broker_service/Observer.java
@@ -12,7 +12,15 @@ public class Observer {
     // теx сервисов, которые ожидают сообщение от брокера
     private final List<String> subscribedServices;
 
+    // В списке будут храниться все сообщения от сервиса-продюсера
+    private final List<String> messages;
+
     public Observer() {
         this.subscribedServices = new ArrayList<>(16);
+        this.messages = new ArrayList<>(16);
     }
+
+    // TODO: Функция подписки на брокер
+    // TODO: Функция добавления сообщения в список
+    // TODO: Функция отправки сообщения во все консюмеры
 }
